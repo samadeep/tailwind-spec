a0554b1ebb03683e9bb509c6272fed29e334b03a
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2022-09-28 18:51:44 +0530

    deploy changes 3

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import herovid from "../../assets/2112.w026.n002.1265B.p1.1265.webp";
+import herovid from "../../assets/2112.w026.n002.1265B.p1.1265.jpg";
 export default function Hero() {
   return (
     <div name="home" className="w-full h-[90vh] top-[90px]">
